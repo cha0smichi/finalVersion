fa5d90eed2bc5df78a9227b9d260fe99c17f1ef8
michi <michidoser@gmx.de>
2023-07-20 17:28:37 +0200

    2

--- MyApplication4/app/src/main/java/com/example/myapplication/ui/settings/SettingsFragment.java
+++ MyApplication4/app/src/main/java/com/example/myapplication/ui/settings/SettingsFragment.java
@@ -1,27 +1,19 @@
 package com.example.myapplication.ui.settings;
 
-
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import com.example.myapplication.R;
 
-import java.text.DecimalFormat;
-import java.util.Calendar;
-import java.util.TimeZone;
 
-public class settingsFragment extends Fragment {
+
+public class SettingsFragment extends Fragment {
+
+        return rootView;
+    }
 }
-
