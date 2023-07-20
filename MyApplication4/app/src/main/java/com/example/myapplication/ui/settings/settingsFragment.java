344ada642053bf50ba32713b366bbcaa8785d11f
michi <michidoser@gmx.de>
2023-07-20 13:42:15 +0200

    2

--- MyApplication4/app/src/main/java/com/example/myapplication/ui/settings/settingsFragment.java
+++ MyApplication4/app/src/main/java/com/example/myapplication/ui/settings/settingsFragment.java
@@ -1,4 +1,27 @@
 package com.example.myapplication.ui.settings;
 
-public class settingsFragment {
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.myapplication.R;
+
+import java.text.DecimalFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class settingsFragment extends Fragment {
 }
+
