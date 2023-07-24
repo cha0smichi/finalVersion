f410c407c9440d6d04729c5c2c35c9a52586f2af
SebastianWeber99 <sebastian.m.weber@web.de>
2023-07-24 23:34:37 +0200

    add new Settings and LanguageManager

--- MyApplication4/app/src/main/java/com/example/myapplication/ui/settings/SettingsFragment.java
+++ MyApplication4/app/src/main/java/com/example/myapplication/ui/settings/SettingsFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,3 +15,19 @@ import androidx.fragment.app.Fragment;
 import com.example.myapplication.R;
 
 public class SettingsFragment extends Fragment {
+
+    private Button languageDE;
+    private Button languageEN;
+    private Button languageHR;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        languageDE = rootView.findViewById(R.id.languageDE);
+        languageEN = rootView.findViewById(R.id.languageEN);
+        languageHR = rootView.findViewById(R.id.languageHR);
+
+        return rootView;
+    }
+}
