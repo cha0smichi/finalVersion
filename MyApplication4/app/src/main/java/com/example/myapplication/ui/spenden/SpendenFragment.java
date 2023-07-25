08ee13b676370aa840575280bbe3759fd85b6994
michi <michidoser@gmx.de>
2023-07-25 22:38:05 +0200

    3

--- MyApplication4/app/src/main/java/com/example/myapplication/ui/spenden/SpendenFragment.java
+++ MyApplication4/app/src/main/java/com/example/myapplication/ui/spenden/SpendenFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -30,8 +31,15 @@ public class SpendenFragment extends Fragment {
     private TextView textView20;
     private TextView textView41;
     private TextView textView42;
+    private Button button6;
+    private Button button5;
+    private Button button4;
+
     private boolean isTextView17Visible = false;
     private boolean isTextView18Visible = false;
+    private boolean isTextView20Visible = false;
+    private boolean isTextView41Visible = false;
+    private boolean isTextView42Visible = false;
 
     @SuppressLint("MissingInflatedId")
     @Nullable
@@ -53,9 +61,19 @@ public class SpendenFragment extends Fragment {
         textView41 = rootView.findViewById(R.id.textView41);
         textView42 = rootView.findViewById(R.id.textView42);
 
-        // Setze die Sichtbarkeit der TextViews auf "gone" zu Beginn
+        button6 = rootView.findViewById(R.id.button6);
+        button5 = rootView.findViewById(R.id.button5);
+        button4 = rootView.findViewById(R.id.button4);
+
+        // Setze die Sichtbarkeit der TextViews und Buttons auf "gone" zu Beginn
         textView17.setVisibility(View.GONE);
         textView18.setVisibility(View.GONE);
+        button6.setVisibility(View.GONE);
+        textView20.setVisibility(View.GONE);
+        button5.setVisibility(View.GONE);
+        textView41.setVisibility(View.GONE);
+        button4.setVisibility(View.GONE);
+        textView42.setVisibility(View.GONE);
 
         headlineTextView2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,6 +93,36 @@ public class SpendenFragment extends Fragment {
             }
         });
 
+        headlineTextView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isTextView20Visible = !isTextView20Visible;
+                updateVisibility(button6, isTextView20Visible);
+                updateVisibility(textView20, isTextView20Visible);
+                updateConstraints();
+            }
+        });
+
+        headlineTextView8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isTextView41Visible = !isTextView41Visible;
+                updateVisibility(button5, isTextView41Visible);
+                updateVisibility(textView41, isTextView41Visible);
+                updateConstraints();
+            }
+        });
+
+        headlineTextView9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isTextView42Visible = !isTextView42Visible;
+                updateVisibility(button4, isTextView42Visible);
+                updateVisibility(textView42, isTextView42Visible);
+                updateConstraints();
+            }
+        });
+
         return rootView;
     }
 
@@ -89,6 +137,36 @@ public class SpendenFragment extends Fragment {
 
     // Methode zum Aktualisieren von Constraints, falls notwendig
     private void updateConstraints() {
-        // Füge hier den Code ein, um die Constraints anzupassen, falls erforderlich
+        // Adjust the constraints based on visibility of the elements
+
+        // For Button 6 and textView20
+        if (isTextView20Visible) {
+            // If textView20 is visible, make Button 6 visible and set its constraints accordingly
+            button6.setVisibility(View.VISIBLE);
+            // Add code to adjust the constraints for Button 6 as needed
+        } else {
+            button6.setVisibility(View.GONE);
+            // Add code to adjust constraints to hide Button 6 if needed
+        }
+
+        // For Button 5 and TextView41
+        if (isTextView41Visible) {
+            // If TextView41 is visible, make Button 5 visible and set its constraints accordingly
+            button5.setVisibility(View.VISIBLE);
+            // Add code to adjust the constraints for Button 5 as needed
+        } else {
+            button5.setVisibility(View.GONE);
+            // Add code to adjust constraints to hide Button 5 if needed
+        }
+
+        // For Button 4 and TextView42
+        if (isTextView42Visible) {
+            // If TextView42 is visible, make Button 4 visible and set its constraints accordingly
+            button4.setVisibility(View.VISIBLE);
+            // Add code to adjust the constraints for Button 4 as needed
+        } else {
+            button4.setVisibility(View.GONE);
+            // Add code to adjust constraints to hide Button 4 if needed
+        }
     }
 }
