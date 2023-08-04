770f2408eb4ee7b68c8a1daf82d7043ba2136d5f
michi <michidoser@gmx.de>
2023-08-04 18:44:36 +0200

    3

--- MyApplication4/app/src/main/java/com/example/myapplication/ui/settings/SettingsFragment.java
+++ MyApplication4/app/src/main/java/com/example/myapplication/ui/settings/SettingsFragment.java
@@ -26,6 +26,17 @@ public class SettingsFragment extends Fragment {
     private ImageButton languageDE;
     private ImageButton languageES;
     private ImageButton languageHR;
+    private ImageButton languageFR;
+    private ImageButton languageIT;
+    private ImageButton languageJA;
+    private ImageButton languageNL;
+    private ImageButton languageRU;
+    private ImageButton languageTR;
+    private ImageButton languageUK;
+    private ImageButton languageEN;
+    private ImageButton languagePL;
+    private ImageButton languageSA;
+    private ImageButton languageCN;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -34,6 +45,17 @@ public class SettingsFragment extends Fragment {
         languageDE = rootView.findViewById(R.id.languageDE);
         languageES = rootView.findViewById(R.id.languageES);
         languageHR = rootView.findViewById(R.id.languageHR);
+        languageFR = rootView.findViewById(R.id.languageFR);
+        languageIT = rootView.findViewById(R.id.languageIT);
+        languageJA = rootView.findViewById(R.id.languageJA);
+        languageNL = rootView.findViewById(R.id.languageNL);
+        languageRU = rootView.findViewById(R.id.languageRU);
+        languageTR = rootView.findViewById(R.id.languageTR);
+        languageUK = rootView.findViewById(R.id.languageUK);
+        languageEN = rootView.findViewById(R.id.languageEN);
+        languagePL = rootView.findViewById(R.id.languagePL);
+        languageSA = rootView.findViewById(R.id.languageSA);
+        languageCN = rootView.findViewById(R.id.languageCN);
 
         LanguageManager languageManager = new LanguageManager(getActivity());
 
@@ -49,11 +71,63 @@ public class SettingsFragment extends Fragment {
             languageManager.updateResource("hr");
             restartActivity();
         });
+        languageFR.setOnClickListener(view -> {
+            languageManager.updateResource("fr");
+            restartActivity();
+        });
 
+        languageIT.setOnClickListener(view -> {
+            languageManager.updateResource("it");
+            restartActivity();
+        });
+        languageJA.setOnClickListener(view -> {
+            languageManager.updateResource("ja ");
+            restartActivity();
+        });
+
+        languageNL.setOnClickListener(view -> {
+            languageManager.updateResource("nl");
+            restartActivity();
+        });
+
+        languageRU.setOnClickListener(view -> {
+            languageManager.updateResource("ru");
+            restartActivity();
+        });
+
+        languageTR.setOnClickListener(view -> {
+            languageManager.updateResource("tu");
+            restartActivity();
+        });
+
+        languageUK.setOnClickListener(view -> {
+            languageManager.updateResource("uk");
+            restartActivity();
+        });
+
+        languageEN.setOnClickListener(view -> {
+            languageManager.updateResource("en");
+            restartActivity();
+        });
+        languagePL.setOnClickListener(view -> {
+            languageManager.updateResource("pl");
+            restartActivity();
+        });
+
+
+        languageSA.setOnClickListener(view -> {
+            languageManager.updateResource("sa");
+            restartActivity();
+        });
+
+        languageCN.setOnClickListener(view -> {
+            languageManager.updateResource("cn");
+            restartActivity();
+        });
         return rootView;
     }
 
-    // Helper method to restart the activity to apply language changes to all components
+    // Helper method to restart the ac6tivity to apply language changes to all components
     private void restartActivity() {
         Intent intent = getActivity().getIntent();
         getActivity().finish();
